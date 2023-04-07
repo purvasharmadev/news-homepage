d30b1878fb3962e7725ff6c71662056af4924594
biz-purva <purva.sharma@bizolutiontech.com>
2023-04-07 17:37:09 +0530

    style: added data

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,41 +1,29 @@
 import { Container, Divider, Paper, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import {sidebar} from "../data"
 
 function SideBar() {
   return (
     <Container >
         <Paper sx={{backgroundColor:'hsl(240, 100%, 5%)',padding:'10px',borderRadius:'0'}} elevation={0}>
-            <Box padding={1}>
+            {
+                sidebar.text.map((item)=>{
+                    return(
+<Box padding={1}>
             <Typography sx={{color:'white',padding:'8px 1rem'}}>
-                Hydrogen VS Electric Cars
+                {item.heading}
             </Typography>
             <Typography sx={{color:'hsl(36, 100%, 99%)',padding:'8px 1rem'}} >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum 
+               {item.paragraph}
                 <Divider  sx={{borderColor:'white',marginTop:'10px'}}  />    
             </Typography>
             </Box>
+                    )
+                })
+            }
             
-           
-            <Box padding={1}>
-            <Typography sx={{color:'white',padding:'8px 1rem'}}>
-                Hydrogen VS Electric Cars
-            </Typography>
-            <Typography sx={{color:'hsl(36, 100%, 99%)',padding:'8px 1rem'}} >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum 
-                <Divider  sx={{borderColor:'white',marginTop:'10px'}}  />    
-            </Typography>
-            </Box>
             
-            <Box padding={1}>
-            <Typography sx={{color:'white',padding:'8px 1rem'}}>
-                Hydrogen VS Electric Cars
-            </Typography>
-            <Typography sx={{color:'hsl(36, 100%, 99%)',padding:'8px 1rem'}} >
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum 
-                <Divider  sx={{borderColor:'white',marginTop:'10px'}}  />    
-            </Typography>
-            </Box>
         </Paper>
     </Container>
   )
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const sidebar = {
+    text:[
+        {
+            heading:'Hydrogen VS Electric Cars',
+            paragraph:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum '
+        },
+        {
+            heading:'Hydrogen VS Electric Cars',
+            paragraph:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum '
+        },
+        {
+            heading:'Hydrogen VS Electric Cars',
+            paragraph:' Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat assumenda laborum '
+        },
+    ]
+}
